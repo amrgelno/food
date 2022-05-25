be62eb246859152da47ca8488cbc0861e52abcff
amrgelno <amrgelno@gmail.com>
2022-05-24 21:44:47 -0700

    Add Food file  commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function start() {
+  document.querySelector(".navbar").style.display = "block";
+  document.querySelector("#menu-bar").style.display = "none";
+  document.querySelector("#exit-bar").style.display = "block";
+}
+function end() {
+  document.querySelector(".navbar").style.display = "none";
+  document.querySelector("#menu-bar").style.display = "block";
+  document.querySelector("#exit-bar").style.display = "none";
+}
+
